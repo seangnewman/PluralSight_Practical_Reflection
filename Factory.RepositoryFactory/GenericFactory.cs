13c6f2a647b863c359fee86f4fd7616c644f2f9d
newmanuevers <seangnewman@msn.com>
2018-10-12 22:58:00 -0700

    completed section 3

---
+++ Factory.RepositoryFactory/GenericFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+
+namespace Factory.RepositoryFactory
+{
+    public static class GenericFactory
+    {
+        public static T Get<T>() where T : class
+        {
+            string resolvedTypeName = ConfigurationManager.AppSettings[typeof(T).ToString()];
+            Type resolvedType = Type.GetType(resolvedTypeName);
+            object instance = Activator.CreateInstance(resolvedType);
+            T result = instance as T;
+            return result;
+
+
+        }
+    }
+}
